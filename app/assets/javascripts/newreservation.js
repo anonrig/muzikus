813de6aae36d3dd7abce4e6d777fc80928e6a31e
ynizipli <ynizipli@sabanciuniv.edu>
2014-07-19 00:39:37 +0300

    Fix indentation and code style.

--- app/assets/javascripts/newreservation.js
+++ app/assets/javascripts/newreservation.js
@@ -1,62 +1,40 @@
 $(document).ready(function(){
-
 	$('.roomselector').change(function() {
 
-	  var id =  $(this).find("option:selected").attr('value');
-	  console.log(id);
+        var id =  $(this).find("option:selected").attr('value'),
+            address = $('#address'),
+            phone = $('#phone'),
+            email = $('#email');
 
-	  var address = $('#address');
-	  var phone = $('#phone');
-	  var email = $('#email');
-	  if (id == 1){ //davul odasi
-	  	address.empty();
-	  	phone.empty();
-	  	email.empty();
+        address.empty();
+        phone.empty();
+        email.empty();
 
-	  	address.append('Kaan KARA');
-	  	phone.append('0534 872 8072');
-	  	email.append('kaankara@sabanciuniv.edu');
-	  } else if (id == 2){ //studyo
-	  	address.empty();
-	  	phone.empty();
-	  	email.empty();
-
-	  	address.append('Yagiz ANDAC, Cagri KARAOGLU');
-	  	phone.append('0537 222 7192, 0553 354 9171');
-	  	email.append('yagizandac@sabanciuniv.edu, hkaraoglu@sabanciuniv.edu');
-	  } else if (id == 3){ // piyano odasi 1
-	  	address.empty();
-	  	phone.empty();
-	  	email.empty();
-
-	  	address.append('Sinem BAYRAKTAR');
-	  	phone.append('0536 384 6791');
-	  	email.append('sinemb@sabanciuniv.edu');
-	  } else if (id == 4){ // piyano odasi 2
-	  	address.empty();
-	  	phone.empty();
-	  	email.empty();
-
-	  	address.append('Iskender YALCINKAYA');
-	  	phone.append('0542 242 0520');
-	  	email.append('iyalcinkaya@sabanciuniv.edu');
-	  } else if (id == 5){ // san odasi
- 		address.empty();
-	  	phone.empty();
-	  	email.empty();
-
-	  	address.append('Meryem Sena BICER');
-	  	phone.append('0536 814 2044');
-	  	email.append('meryemsenab@sabanciuniv.edu');
-	  } else { //hangar
-	  	address.empty();
-	  	phone.empty();
-	  	email.empty();
-
-	  	address.append('Jankat YILMAZ');
-	  	phone.append('0533 431 3445');
-	  	email.append('jankaty@sabanciuniv.edu');
-	  }
+        if (id == 1){ //davul odasi
+            address.append('Kaan KARA');
+            phone.append('0534 872 8072');
+            email.append('kaankara@sabanciuniv.edu');
+        } else if (id == 2){ //studyo
+            address.append('Yagiz ANDAC, Cagri KARAOGLU');
+            phone.append('0537 222 7192, 0553 354 9171');
+            email.append('yagizandac@sabanciuniv.edu, hkaraoglu@sabanciuniv.edu');
+        } else if (id == 3){ // piyano odasi 1
+            address.append('Sinem BAYRAKTAR');
+            phone.append('0536 384 6791');
+            email.append('sinemb@sabanciuniv.edu');
+        } else if (id == 4){ // piyano odasi 2
+            address.append('Iskender YALCINKAYA');
+            phone.append('0542 242 0520');
+            email.append('iyalcinkaya@sabanciuniv.edu');
+        } else if (id == 5){ // san odasi
+            address.append('Meryem Sena BICER');
+            phone.append('0536 814 2044');
+            email.append('meryemsenab@sabanciuniv.edu');
+        } else { //hangar
+            address.append('Jankat YILMAZ');
+            phone.append('0533 431 3445');
+            email.append('jankaty@sabanciuniv.edu');
+        }
 	});
 
 	$('#submitreservation').on('click', function(event){
@@ -64,11 +42,10 @@ $(document).ready(function(){
 		$('#contactError').addClass('hidden').empty().append("<strong>Oh snap!</strong> ");
 		$('#contactSuccess').addClass('hidden').empty().append("<strong>Success!</strong> ");
 		
-		var f = $(this).parent().parent();
-
-		var whichDay = $('#whichday').find("option:selected").attr('value');;
-		var startHour = $('#starthour').find("option:selected").text();
-		var hourCount = $('#hourcount').find("option:selected").attr('value');;
+		var f = $(this).parent().parent(),
+            whichDay = $('#whichday').find("option:selected").attr('value'),
+            startHour = $('#starthour').find("option:selected").text(),
+            hourCount = $('#hourcount').find("option:selected").attr('value');;
 
 		$('this').attr("disabled", true);
 
@@ -102,5 +79,4 @@ $(document).ready(function(){
 	});
 	
 	$('.tooltip-test').tooltip();
-
 });
