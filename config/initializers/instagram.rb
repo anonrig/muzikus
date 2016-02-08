1db995d21499af253d764263d1f6b0514febe9cb
atayenel <atakanyenel@gmail.com>
2016-02-08 21:52:12 +0200

    added instagram twitter api

---
+++ config/initializers/instagram.rb
@@ -0,0 +1,8 @@
+require "instagram"
+
+Instagram.configure do |config|
+
+config.client_id="5b26bf95babc407fb14fc6d08437d2fb"
+config.access_token="1702172116.5b26bf9.47280f29ce004e9c9284ec35e1d0b572"
+
+end
