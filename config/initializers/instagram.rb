4182a47dc00e173937255b8ef9181f806c710711
atayenel <atakanyenel@gmail.com>
2016-02-08 22:53:01 +0200

    fix heroku config, delete comments

--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -2,7 +2,7 @@ require "instagram"
 
 Instagram.configure do |config|
 
-config.client_id="5b26bf95babc407fb14fc6d08437d2fb"
-config.access_token="1702172116.5b26bf9.47280f29ce004e9c9284ec35e1d0b572"
+config.client_id=ENV["instagram_id"]
+config.access_token=ENV["instagram_secret"]
 
 end
