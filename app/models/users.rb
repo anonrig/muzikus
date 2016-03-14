7f0b58662a0afe3463edb4f0a1258d609aef5e64
atayenel <atakanyenel@gmail.com>
2016-03-14 06:26:04 +0200

    fix strong parameter in rails 4

--- app/models/users.rb
+++ app/models/users.rb
@@ -4,7 +4,7 @@ class Users < ActiveRecord::Base
 	has_many :budgets
 	belongs_to :scout, :class_name => 'Scouts', :foreign_key => "id"
 	def self.from_omniauth(auth)
-	  where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
+	  where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
 	    user.provider = auth.provider
 	    user.uid = auth.uid
 	    user.name = auth.info.name
