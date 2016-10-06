ec00a9ef29e3518e424b4f7084b8076c7551004f
atayenel <atakanyenel@gmail.com>
2016-10-06 16:19:00 +0300

    update room info

--- app/assets/javascripts/newreservation.js
+++ app/assets/javascripts/newreservation.js
@@ -11,29 +11,27 @@ $(document).ready(function(){
         email.empty();
 
 if (id == 1){ //davul odasi
-    address.append('Berkin Özbatir');
-    phone.append('0534 357 6588');
-    email.append('berkinozbatir@sabanciuniv.edu');
+    address.append('Burak Aksar');
+    phone.append('0507 310 2235');
+    email.append('burakaksar@sabanciuniv.edu');
 } else if (id == 2){ //studyo
-    address.append('Kartal Batuhan Ucar, Ege Sarac');
-    phone.append('0537 598 9192, 0543 978 0607');
-    email.append('kartalucar@sabanciuniv.edu, esarac@sabanciuniv.edu');
+    address.append('Erkin Alacamli, Orhun Baris');
+    phone.append('0535 0535 41 73 , 0532 345 5468');
+    email.append('erkinalacamli@sabanciuniv.edu, orhunbaris@sabanciuniv.edu');
 } else if (id == 3){ // piyano odasi 1
-    address.append('Sevval Calis');
-    phone.append('0534 438 1116');
-    email.append('calissevval@sabanciuniv.edu');
-} else if (id == 4){ // piyano odasi 2
-    address.append('Iskender Yalcinkaya');
-    phone.append('0542 242 0520');
-    email.append('iyalcinkaya@sabanciuniv.edu');
-} else if (id == 5){ // san odasi
-    address.append('Isinsu Tastan');
-    phone.append('0554 776 7718');
-    email.append('tastanisinsu@sabanciuniv.edu');
+    address.append('Ege Sarac');
+    phone.append('0543 978 0607');
+    email.append('esarac@sabanciuniv.edu');
+} 
+ 
+else if (id == 5){ // san odasi
+    address.append('Rabia Tuncbilek');
+    phone.append('0537 311 5401');
+    email.append('rabiatuncbilek@sabanciuniv.edu');
 } else { //hangar
-    address.append('Berk Can Altindag');
-    phone.append('0535 221 6994');
-    email.append('acan@sabanciuniv.edu');
+    address.append('Kartal Batuhan Ucar, Kenan Akpınar');
+    phone.append('0537 598 9192 , 0506 113 1912 ');
+    email.append('kartalucar@sabanciuniv.edu', 'kenanakpınar@sabanciuniv.edu');
 }
 	});
 
