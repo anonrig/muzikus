7f0592373c91effef4300bf262a05983cf29609e
atayenel <atakanyenel@gmail.com>
2016-10-13 23:11:06 +0300

    change secret to env var

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '151206855076651', '0968d5b3cf0f8ec4b4989f8234d3f8fb', {:scope => "email, user_birthday, publish_actions"}
+  provider :facebook, ENV["facebook_id"], ENV["facebook_secret"], {:scope => "email, user_birthday, publish_actions"}
 end
