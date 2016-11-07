cec8772de8a7f8a1b0a219b594a0b61380475080
atakanyenel <atakanyenel@gmail.com>
2016-11-07 20:13:14 +0300

    update room schema

--- db/schema.rb
+++ db/schema.rb
@@ -13,87 +13,90 @@
 
 ActiveRecord::Schema.define(version: 20131125151903) do
 
-  create_table "budgets", force: true do |t|
-    t.string   "amount"
-    t.string   "reason"
-    t.string   "user_id"
+  create_table "budgets", force: :cascade do |t|
+    t.string   "amount",      limit: 255
+    t.string   "reason",      limit: 255
+    t.string   "user_id",     limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "budget_type"
+    t.string   "budget_type", limit: 255
   end
 
-  create_table "muzikususers", force: true do |t|
-    t.string   "email"
+  create_table "muzikususers", force: :cascade do |t|
+    t.string   "email",      limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "sid"
-    t.boolean  "isyk"
-    t.boolean  "ismyk"
-    t.boolean  "isdavul"
-    t.boolean  "isworkshop"
+    t.integer  "sid",        limit: 4
+    t.boolean  "isyk",       limit: 1
+    t.boolean  "ismyk",      limit: 1
+    t.boolean  "isdavul",    limit: 1
+    t.boolean  "isworkshop", limit: 1
   end
 
-  create_table "reservations", force: true do |t|
-    t.integer  "user_id"
-    t.integer  "room_id"
+  create_table "reservations", force: :cascade do |t|
+    t.integer  "user_id",    limit: 4
+    t.integer  "room_id",    limit: 4
     t.datetime "start_date"
     t.datetime "end_date"
-    t.boolean  "isCanceled"
+    t.boolean  "isCanceled", limit: 1
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "day"
-    t.text     "info"
-    t.integer  "hour"
+    t.integer  "day",        limit: 4
+    t.text     "info",       limit: 65535
+    t.integer  "hour",       limit: 4
   end
 
-  create_table "rooms", force: true do |t|
-    t.string   "name"
+  create_table "rooms", force: :cascade do |t|
+    t.string   "name",         limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "officer",      limit: 255, null: false
+    t.string   "officer_num",  limit: 255, null: false
+    t.string   "officer_mail", limit: 255, null: false
   end
 
-  create_table "roomschedules", force: true do |t|
-    t.string   "name"
+  create_table "roomschedules", force: :cascade do |t|
+    t.string   "name",       limit: 255
     t.datetime "start_date"
     t.datetime "end_date"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "room_id"
-    t.integer  "day"
+    t.integer  "room_id",    limit: 4
+    t.integer  "day",        limit: 4
   end
 
-  create_table "scouts", force: true do |t|
-    t.string   "BirinciEns"
-    t.string   "BirinciSev"
-    t.string   "BirinciYil"
-    t.string   "IkinciEns"
-    t.string   "IkinciSev"
-    t.string   "IkinciYil"
-    t.string   "VokalYetenek"
-    t.string   "SahneTecrubesi"
-    t.text     "UyeGruplar"
-    t.text     "SevdiginGruplar"
-    t.integer  "user_id"
+  create_table "scouts", force: :cascade do |t|
+    t.string   "BirinciEns",      limit: 255
+    t.string   "BirinciSev",      limit: 255
+    t.string   "BirinciYil",      limit: 255
+    t.string   "IkinciEns",       limit: 255
+    t.string   "IkinciSev",       limit: 255
+    t.string   "IkinciYil",       limit: 255
+    t.string   "VokalYetenek",    limit: 255
+    t.string   "SahneTecrubesi",  limit: 255
+    t.text     "UyeGruplar",      limit: 65535
+    t.text     "SevdiginGruplar", limit: 65535
+    t.integer  "user_id",         limit: 4
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "supports", force: true do |t|
-    t.string   "name"
-    t.string   "email"
-    t.string   "subject"
-    t.text     "message"
+  create_table "supports", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.string   "email",      limit: 255
+    t.string   "subject",    limit: 255
+    t.text     "message",    limit: 65535
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "users", force: true do |t|
-    t.string   "provider"
-    t.string   "uid"
-    t.string   "name"
-    t.string   "email"
-    t.string   "sabancimail"
-    t.string   "oauth_token"
+  create_table "users", force: :cascade do |t|
+    t.string   "provider",         limit: 255
+    t.string   "uid",              limit: 255
+    t.string   "name",             limit: 255
+    t.string   "email",            limit: 255
+    t.string   "sabancimail",      limit: 255
+    t.string   "oauth_token",      limit: 255
     t.datetime "oauth_expires_at"
     t.datetime "created_at"
     t.datetime "updated_at"
