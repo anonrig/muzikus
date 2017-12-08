8bf0424a0471b7935173757f72b952b696573f8f
sinantplgl <topaloglusinan@sabanciuniv.edu>
2017-12-08 13:08:32 +0300

    login

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,10 +1,6 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV["facebook_id"], ENV["facebook_secret"], {
-  			scope: 'https://www.googleapis.com/auth/userinfo.profile, https://www.googleapis.com/auth/userinfo.email, https://mail.google.com/, https://www.googleapis.com/auth/admin.directory.user.readonly',
-            prompt: 'select_account consent',
-            access_type: 'offline',
-	        callback_path: '/auth/google_oauth2/callback'}
+  provider :facebook, ENV["facebook_id"], ENV["facebook_secret"], {:scope => "email, user_birthday, publish_actions"}
 
 end
