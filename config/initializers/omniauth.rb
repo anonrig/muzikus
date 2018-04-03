4894cd30d19b26bb7cfbcf81fe64a19ef136f2bd
Sinan Topaloglu <topaloglusinan@sabanciuniv.edu>
2018-04-03 12:52:24 +0300

    fix for oauth error

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-	provider :google_oauth2, ENV["GOOGLE_ID"], ENV["GOOGLE_SECRET"], 
+	provider :google_oauth2, ENV["GOOGLE_ID"], ENV["GOOGLE_SECRET"], provider_ignores_state: true, 
 	{ 
 		:scope => 'email,profile',
 		:hd => 'sabanciuniv.edu'
