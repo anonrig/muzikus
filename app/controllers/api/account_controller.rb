431f8b5675dcb9f7d0e20055e19040cfcaaca2a0
Sinan Topaloglu <topaloglusinan@sabanciuniv.edu>
2019-12-07 14:54:39 +0300

    response fix

--- app/controllers/api/account_controller.rb
+++ app/controllers/api/account_controller.rb
@@ -1,7 +1,7 @@
 class Api::AccountController < ApiController
     def get
         response = ApiModels::BaseApiResponse.new
-        response.data = current_user
+        response.data = current_user.parse_response
         render json: response
     end
 end
