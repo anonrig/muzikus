72285254f8d99215e065f8166089df9ed1d039af
Yagiz Nizipli <yagiz@nizipli.com>
2021-09-30 12:55:25 -0400

    fix: add alumni.sabanciuniv.edu to the approved list

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
 	provider :google_oauth2, ENV["GOOGLE_ID"], ENV["GOOGLE_SECRET"], 
 	{ 
 		scope: 'userinfo.email, userinfo.profile',
-		hd: 'sabanciuniv.edu',
+		hd: %w(sabanciuniv.edu alumni.sabanciuniv.edu),
 		prompt: 'select_account'
 	}
 end
