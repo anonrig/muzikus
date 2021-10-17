91739e232188a70239f7b7be1b2b5df3939c6e5a
Yagiz Nizipli <yagiz@nizipli.com>
2021-10-17 13:28:20 -0400

    fix: add nizipli.com for testing

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,9 +2,8 @@ Rails.application.config.middleware.use OmniAuth::Builder do
 	provider :google_oauth2, ENV["GOOGLE_ID"], ENV["GOOGLE_SECRET"], 
 	{ 
 		scope: 'userinfo.email, userinfo.profile',
-		hd: %w(sabanciuniv.edu alumni.sabanciuniv.edu),
-		prompt: 'select_account'
+		hd: %w(sabanciuniv.edu alumni.sabanciuniv.edu nizipli.com)
 	}
 end
 
-OmniAuth.config.allowed_request_methods = [:post, :get]
+OmniAuth.config.allowed_request_methods = [:post]
