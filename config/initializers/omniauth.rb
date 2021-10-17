0c1e243bea9269652b3833385eeb6e01780eb47b
Yagiz Nizipli <yagiz@nizipli.com>
2021-10-17 13:56:35 -0400

    fix: add token verifier

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,19 @@
+# Derived from https://github.com/cookpad/omniauth-rails_csrf_protection/blob/master/lib/omniauth/rails_csrf_protection/token_verifier.rb
+# This specific implementation has been pared down and should not be taken as the most correct way to do this.
+class TokenVerifier
+  include ActiveSupport::Configurable
+  include ActionController::RequestForgeryProtection
+
+  def call(env)
+    @request = ActionDispatch::Request.new(env.dup)
+    raise OmniAuth::AuthenticityError unless verified_request?
+  end
+
+  private
+  attr_reader :request
+  delegate :params, :session, to: :request
+end
+
 Rails.application.config.middleware.use OmniAuth::Builder do
 	provider :google_oauth2, ENV["GOOGLE_ID"], ENV["GOOGLE_SECRET"], 
 	{ 
@@ -7,3 +23,4 @@ Rails.application.config.middleware.use OmniAuth::Builder do
 end
 
 OmniAuth.config.allowed_request_methods = [:post]
+OmniAuth.config.request_validation_phase = TokenVerifier.new
